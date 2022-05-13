eb2b08e905c43fc797f27cbf4463e016026e9fcf
Abzed <ibraabzed@gmail.com>
2022-05-13 11:45:59 +0300

    commit tree 60043aca72376e695c9975f9a2d912798b0ed281

--- index.js
+++ index.js
@@ -1,1 +1,75 @@
-// Your code here
+class Polygon{
+    constructor(array = []){
+        this.array = array
+    }
+    
+    get countSides(){
+        return this.array.length;
+    }
+
+    get perimeter(){
+        return this.array.reduce((partialSum, a) => partialSum + a, 0);
+    }
+}
+const polygon = new Polygon( [ 5, 5, 5 ] )
+console.log(polygon);
+
+
+class Triangle extends Polygon{
+
+    constructor(...args){
+        super(...args)
+    }
+
+    get isValid(){
+        const result = this.array.every(element => {
+            if (element === this.array[0]) {
+              return true;
+            }
+          });
+        
+          return result;
+    }
+}
+
+const triangle = new Triangle( [ 5, 5, 5 ] )
+const triangle2 = new Triangle( [ 15, 10, 1 ] )
+
+console.log(triangle);
+console.log(triangle2);
+
+
+class Square extends Polygon{
+
+    constructor(...args){
+        super(...args)
+    }
+
+    get area(){
+        return this.array[0] * this.array[1]
+    }
+
+    get isValid(){
+        const result = this.array.every(element => {
+            if (element == this.array[0]) {
+                return true
+            }
+        })
+
+        return result
+    }
+}
+
+let square = new Square( [ 5, 5, 5, 6 ] )
+
+console.log(square);
+
+
+
+
+
+
+
+
+
+
